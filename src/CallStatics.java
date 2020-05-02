88e66523a4f5eef38bf561a8e7b3564326f08f27
rsena <rsenaulgur@gmail.com>
2020-05-02 15:19:07 -0400

    Initial commit

---
+++ src/CallStatics.java
@@ -0,0 +1,10 @@
+public class CallStatics {
+
+    public static void main(String[] args) {
+
+        System.out.println(Summary.daysInAYear);
+        int hours = Summary.hourCountInAYear();
+
+        // notice that there is no need for creating an object
+    }
+}
